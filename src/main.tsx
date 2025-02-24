f13155c11e13f666b646bd94175357c519df6928
saeid-tavazani <saeidtavazani@gmail.com>
2025-02-24 23:30:53 +0330

    feat: integrate React Query for data fetching with QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import App from './App.tsx';
 import './index.css';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={App} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={App} />
+    </QueryClientProvider>
   </StrictMode>,
 );
