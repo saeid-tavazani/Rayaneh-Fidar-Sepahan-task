51030eec718120360b4c6cce6f577778a72d8758
saeid-tavazani <saeidtavazani@gmail.com>
2025-02-25 13:51:05 +0330

    feat: update Home component with placeholder content and styling for improved layout

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="container mx-auto px-4 flex items-center justify-center min-h-[700px]">
+      <h1 className="text-3xl font-bold">This is a placeholder page to complete the project route.</h1>
+    </div>
+  );
 };
 
 export default Home;
