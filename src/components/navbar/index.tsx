427965cb0ebc041cd1c32fa02b26db520198400a
saeid-tavazani <saeidtavazani@gmail.com>
2025-02-25 23:19:44 +0330

    feat: update Navbar component to display dynamic cart item count

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,9 @@
 import { CiSearch, CiShoppingBasket, CiUser } from 'react-icons/ci';
 import { IoIosMenu } from 'react-icons/io';
+import { useCartContext } from '../../context/CartContext';
 import NavItem from './NavItem';
 import Search from './Search';
+
 const items = [
   {
     text: 'Home',
@@ -26,6 +28,8 @@ const items = [
 ];
 
 const Navbar = () => {
+  const { cartItems } = useCartContext();
+
   return (
     <header className="w-full flex flex-col">
       <nav className="container mx-auto px-4 flex items-center justify-between h-[60px] md:h-[100px] gap-3">
@@ -42,9 +46,11 @@ const Navbar = () => {
           <CiSearch className="hidden md:block" size={24} />
           <div className="relative">
             <CiShoppingBasket size={24} />
-            <span className="absolute -bottom-1 -right-1 bg-primary-200 rounded-full size-4 text-white flex items-center justify-center text-xs">
-              5
-            </span>
+            {cartItems.length > 0 && (
+              <span className="absolute -bottom-1 -right-1 bg-primary-200 rounded-full size-4 text-white flex items-center justify-center text-xs">
+                {cartItems.length}
+              </span>
+            )}
           </div>
           <CiUser size={24} />
         </div>
