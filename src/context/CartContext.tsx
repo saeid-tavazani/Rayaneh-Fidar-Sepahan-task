a510a16d7370024db76cf58e4af56c8e1fce9169
saeid-tavazani <saeidtavazani@gmail.com>
2025-02-26 00:53:38 +0330

    feat: optimize CartContext with useCallback for add and remove product functions

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext } from 'react';
+import { createContext, ReactNode, useCallback, useContext } from 'react';
 import { useLocalStorage } from '../hook/useLocalStorage';
 
 type CartProviderProps = {
@@ -15,39 +15,37 @@ type CartContextType = {
   cartItems: CartItem[];
 };
 
-const CartContext = createContext({} as CartContextType);
+const CartContext = createContext<CartContextType | undefined>(undefined);
 
-export const useCartContext = () => {
-  return useContext(CartContext);
+export const useCartContext = (): CartContextType => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCartContext must be used within a CartProvider');
+  }
+  return context;
 };
 
 export const CartProvider = ({ children }: CartProviderProps) => {
   const [cartItems, setCartItems] = useLocalStorage<CartItem[]>('shoppingCart', []);
 
-  const addProductToCart = (id: number) => {
-    setCartItems(currItems => {
-      if (currItems.find(item => item.id === id) == null) {
-        return [...currItems, { id }];
-      }
-      return currItems;
-    });
-  };
-
-  const removeProductFromCart = (id: number) => {
-    setCartItems(currItems => {
-      return currItems.filter(item => item.id !== id);
-    });
-  };
-
-  return (
-    <CartContext.Provider
-      value={{
-        addProductToCart,
-        removeProductFromCart,
-        cartItems,
-      }}
-    >
-      {children}
-    </CartContext.Provider>
+  const addProductToCart = useCallback(
+    (id: number) => {
+      setCartItems(prevItems => {
+        if (!prevItems.some(item => item.id === id)) {
+          return [...prevItems, { id }];
+        }
+        return prevItems;
+      });
+    },
+    [setCartItems],
   );
+
+  const removeProductFromCart = useCallback(
+    (id: number) => {
+      setCartItems(prevItems => prevItems.filter(item => item.id !== id));
+    },
+    [setCartItems],
+  );
+
+  return <CartContext.Provider value={{ addProductToCart, removeProductFromCart, cartItems }}>{children}</CartContext.Provider>;
 };
