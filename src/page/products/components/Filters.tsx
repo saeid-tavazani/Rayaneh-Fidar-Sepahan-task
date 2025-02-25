702517859007025770f0cafbc9a883b585d484e7
saeid-tavazani <saeidtavazani@gmail.com>
2025-02-26 00:40:55 +0330

    feat: implement CategoryContext for managing category state in Filters component

--- src/page/products/components/Filters.tsx
+++ src/page/products/components/Filters.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 import { VscSettings } from 'react-icons/vsc';
 import { Accordion, AccordionContent, AccordionHeaderItem, AccordionItem } from '../../../components/Accordion';
 import Button from '../../../components/Button';
@@ -12,24 +12,47 @@ interface CategoryResponse extends ResTypes {
   categories: string[];
 }
 
-const Filters = () => {
-  const { data, error, isLoading } = useQuery({ queryKey: ['category'], queryFn: () => getData<CategoryResponse>('/category') });
+type CartContextType = {
+  categories: string[] | undefined;
+  isLoading: boolean;
+  handleCategory: (category: string | null) => void;
+  category: string | null;
+};
+
+const CategoryContext = createContext<CartContextType | undefined>(undefined);
+
+export const useCategoryContext = () => {
+  const context = useContext(CategoryContext);
+  if (!context) {
+    throw new Error('useCategoryContext  must be used within a CategoryContext .Provider');
+  }
+  return context;
+};
+
+const Filters = ({ handleCategory, category }: { handleCategory: (value: string | null) => void; category: string | null }) => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['category'],
+    queryFn: () => getData<CategoryResponse>('/category'),
+  });
   const { isSm } = useWindowResize();
 
   if (error) return <p>{error.message}</p>;
 
   return (
-    <section className="col-span-8 md:col-span-2 grid gap-2.5">
-      {isSm ? (
-        <MobileFilter isLoading={isLoading} categories={data?.categories} />
-      ) : (
-        <Management isLoading={isLoading} categories={data?.categories} />
-      )}
-    </section>
+    <CategoryContext.Provider
+      value={{
+        isLoading,
+        categories: data?.categories,
+        handleCategory,
+        category,
+      }}
+    >
+      <section className="col-span-8 md:col-span-2 grid gap-2.5">{isSm ? <MobileFilter /> : <Management />}</section>
+    </CategoryContext.Provider>
   );
 };
 
-const MobileFilter = ({ categories, isLoading }: { categories?: string[]; isLoading: boolean }) => {
+const MobileFilter = () => {
   const [isVisible, setIsVisible] = useState(false);
 
   return (
@@ -49,7 +72,7 @@ const MobileFilter = ({ categories, isLoading }: { categories?: string[]; isLoad
             className="fixed inset-0 w-screen h-screen bg-white z-50"
           >
             <div className="container px-4 mx-auto mt-5">
-              <Management isLoading={isLoading} categories={categories} />
+              <Management />
               <Button onClick={() => setIsVisible(false)} className="text-primary mt-6 px-5">
                 Close
               </Button>
@@ -61,39 +84,57 @@ const MobileFilter = ({ categories, isLoading }: { categories?: string[]; isLoad
   );
 };
 
-const Management = ({ categories, isLoading }: { categories?: string[]; isLoading: boolean }) => (
-  <>
-    <div className="w-full flex items-center justify-between">
-      <span className="text-xl font-medium">Filters</span>
-      <span className="text-base font-normal text-primary cursor-pointer">Clear all</span>
-    </div>
-    {isLoading && <SkeletonLoader />}
-    {categories && <Category categories={categories} />}
-  </>
-);
+const Management = () => {
+  const { isLoading, handleCategory } = useCategoryContext();
+
+  return (
+    <>
+      <div className="w-full flex items-center justify-between">
+        <span className="text-xl font-medium">Filters</span>
+        <span className="text-base font-normal text-primary cursor-pointer" onClick={() => handleCategory(null)}>
+          Clear all
+        </span>
+      </div>
+      {isLoading ? <SkeletonLoader /> : <Category />}
+    </>
+  );
+};
 
 const SkeletonLoader = () => (
-  <div className="flex flex-col gap-3 mt-6 ">
+  <div className="flex flex-col gap-3 mt-6">
     {Array.from({ length: 5 }).map((_, index) => (
       <Skeleton key={index} className="w-full h-4 bg-gray-60" />
     ))}
   </div>
 );
 
-const Category = ({ categories }: { categories: string[] }) => (
-  <Accordion type="single" collapsible defaultValue="category">
-    <AccordionItem value="category" className="text-xl font-light">
-      <AccordionHeaderItem className="mb-2.5">Category</AccordionHeaderItem>
-      <AccordionContent className="flex flex-col gap-2.5">
-        {categories.map(item => (
-          <label key={item} className="flex items-center gap-2.5 cursor-pointer">
-            <input className="size-5 rounded-sm" name="category" type="checkbox" />
-            {item}
-          </label>
-        ))}
-      </AccordionContent>
-    </AccordionItem>
-  </Accordion>
-);
+const Category = () => {
+  const { handleCategory, categories, category } = useCategoryContext();
+
+  return (
+    <Accordion type="single" collapsible defaultValue="category">
+      <AccordionItem value="category" className="text-xl font-light">
+        <AccordionHeaderItem className="mb-2.5">Category</AccordionHeaderItem>
+        <AccordionContent className="flex flex-col gap-2.5">
+          {categories?.map(item => (
+            <label key={item} className="flex items-center gap-2.5 cursor-pointer">
+              <input
+                onChange={e => {
+                  if (e.currentTarget.checked) handleCategory(item);
+                  else handleCategory(null);
+                }}
+                className="size-5 rounded-sm"
+                name="category"
+                type="checkbox"
+                checked={category == item}
+              />
+              {item}
+            </label>
+          ))}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
 
 export default Filters;
