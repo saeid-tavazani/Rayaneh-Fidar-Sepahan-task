67b063312940481a407bc933cb3f2d79e10c9994
saeid-tavazani <saeidtavazani@gmail.com>
2025-02-25 22:51:16 +0330

    feat: remove redundant background color from Skeleton component for cleaner styling

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import React from 'react';
 
 const Skeleton = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => {
-  return <div className={clsx('animate-pulse rounded-md bg-gray-90', className)} {...props} />;
+  return <div className={clsx('animate-pulse rounded-md', className)} {...props} />;
 };
 
 export default Skeleton;
